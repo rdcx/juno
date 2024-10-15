35cdedb15a47549fc842366eed64d5fc3fe6c55f
Ross <rdchater@gmail.com>
2024-10-15 17:57:14 +0100

    fix main

--- cmd/api/run/main.go
+++ cmd/api/run/main.go
@@ -9,18 +9,6 @@ import (
 	nodeRepo "juno/pkg/api/node/repo/mysql"
 	nodeSvc "juno/pkg/api/node/service"
 
-	balancerHandler "juno/pkg/api/balancer/handler"
-	balancerMig "juno/pkg/api/balancer/migration/mysql"
-	balancerPolicy "juno/pkg/api/balancer/policy"
-	balancerRepo "juno/pkg/api/balancer/repo/mysql"
-	balancerSvc "juno/pkg/api/balancer/service"
-
-	assignmentHandler "juno/pkg/api/assignment/handler"
-	assignmentMig "juno/pkg/api/assignment/migration/mysql"
-	assignmentPolicy "juno/pkg/api/assignment/policy"
-	assignmentRepo "juno/pkg/api/assignment/repo/mysql"
-	assignmentSvc "juno/pkg/api/assignment/service"
-
 	userHandler "juno/pkg/api/user/handler"
 	userMig "juno/pkg/api/user/migration/mysql"
 	userPolicy "juno/pkg/api/user/policy"
@@ -37,7 +25,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func setupDatabases() (*sql.DB, *sql.DB, *sql.DB, *sql.DB) {
+func setupDatabases() (*sql.DB, *sql.DB) {
 	nodeDB, err := sql.Open("mysql", "root:juno@tcp(localhost:3306)/node?parseTime=true")
 
 	if err != nil {
@@ -62,31 +50,7 @@ func setupDatabases() (*sql.DB, *sql.DB, *sql.DB, *sql.DB) {
 		panic(err)
 	}
 
-	balancerDB, err := sql.Open("mysql", "root:juno@tcp(localhost:3306)/balancer?parseTime=true")
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = balancerMig.ExecuteMigrations(balancerDB)
-
-	if err != nil {
-		panic(err)
-	}
-
-	assignmentDB, err := sql.Open("mysql", "root:juno@tcp(localhost:3306)/assignment?parseTime=true")
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = assignmentMig.ExecuteMigrations(assignmentDB)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return nodeDB, userDB, balancerDB, assignmentDB
+	return nodeDB, userDB
 }
 
 func main() {
@@ -96,7 +60,7 @@ func main() {
 
 	flag.Parse()
 
-	nodeDB, userDB, balancerDB, assignmentDB := setupDatabases()
+	nodeDB, userDB := setupDatabases()
 
 	logger := logrus.New()
 
@@ -114,22 +78,10 @@ func main() {
 	authSvc := authSvc.New(logger, userSvc)
 	authHandler := authHandler.New(logger, authSvc)
 
-	balancerRepo := balancerRepo.New(balancerDB)
-	balancerSvc := balancerSvc.New(balancerRepo)
-	balancerPolicy := balancerPolicy.New()
-	balancerHandler := balancerHandler.New(logger, balancerPolicy, balancerSvc)
-
-	assignmentRepo := assignmentRepo.New(assignmentDB)
-	assignmentSvc := assignmentSvc.New(assignmentRepo)
-	assignmentPolicy := assignmentPolicy.New()
-	assignmentHandler := assignmentHandler.New(logger, assignmentPolicy, assignmentSvc)
-
 	r := router.New(
 		nodeHandler,
 		userHandler,
 		authHandler,
-		balancerHandler,
-		assignmentHandler,
 	)
 
 	r.Run(":" + portFlag)
--- pkg/api/router/router.go
+++ pkg/api/router/router.go
@@ -1,9 +1,7 @@
 package router
 
 import (
-	"juno/pkg/api/assignment"
 	"juno/pkg/api/auth"
-	"juno/pkg/api/balancer"
 	"juno/pkg/api/middleware"
 	"juno/pkg/api/node"
 	"juno/pkg/api/user"
@@ -18,8 +16,6 @@ func New(
 	nodeHandler node.Handler,
 	userHandler user.Handler,
 	authHandler auth.Handler,
-	balancerHandler balancer.Handler,
-	assignmentHandler assignment.Handler,
 ) *gin.Engine {
 	r := gin.Default()
 
@@ -52,17 +48,6 @@ func New(
 		authGroup.POST("/nodes", nodeHandler.Create)
 		authGroup.PUT("/nodes", nodeHandler.Update)
 		authGroup.DELETE("/nodes/:id", nodeHandler.Delete)
-
-		authGroup.GET("/balancers", balancerHandler.List)
-		authGroup.GET("/balancers/:id", balancerHandler.Get)
-		authGroup.POST("/balancers", balancerHandler.Create)
-		authGroup.PUT("/balancers", balancerHandler.Update)
-		authGroup.DELETE("/balancers/:id", balancerHandler.Delete)
-
-		authGroup.GET("/nodes/:id/assignments", assignmentHandler.ListByNodeID)
-		authGroup.PUT("/assignments/:id", assignmentHandler.Update)
-		authGroup.POST("/assignments", assignmentHandler.Create)
-		authGroup.DELETE("/assignments/:id", assignmentHandler.Delete)
 	}
 
 	return r
